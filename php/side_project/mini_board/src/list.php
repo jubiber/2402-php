e98486005be15924000818fab2dccba59662aa19
jubiber <myemail@example.com>
2024-04-22 22:44:22 +0900

    240422

--- php/side_project/mini_board/src/list.php
+++ php/side_project/mini_board/src/list.php
@@ -1,28 +1,25 @@
 <?php
 // 설정 정보
-// 설정파일 호출하기!
-require_once($_SERVER["DOCUMENT_ROOT"]."/config.php"); 
+require_once($_SERVER["DOCUMENT_ROOT"]."/config.php"); // 설정 파일 호출
 require_once(FILE_LIB_DB); // DB관련 라이브러리
 $list_cnt = 5; // 한 페이지 최대 표시 수
-$page_num = 1; // 페이지 번호 초기화 (기본적으로 1로 셋팅되도록 해줌)
+$page_num = 1; // 페이지 번호 초기화
 
 try {
-    //DB Connect
-    $conn = my_db_conn(); //connection 함수 호출
-
+    // DB Connect
+    $conn = my_db_conn(); // connection 함수 호출
+    
     // 파라미터에서 page 획득
-    $page_num = isset($_GET["page"]) ? $_GET["page"] : $page_num; 
+    $page_num = isset($_GET["page"]) ? $_GET["page"] : $page_num;
 
     // 게시글 수 조회
     $result_board_cnt = db_select_boards_cnt($conn);
-    // TODO : 나중에 추가
 
     // 페이지 관련 설정 셋팅
     $max_page_num = ceil($result_board_cnt / $list_cnt); // 최대 페이지 수
-    $offset = $list_cnt * ($page_num - 1); // OFFSET
-    $prev_page_num = ($page_num - 1) < 1 ? 1 : ($page_num -1); // 이전 버튼 페이지 번호
-    $next_page_num = ($page_num + 1) > $max_page_num ? $max_page_num  : ($page_num + 1); // 다음 버튼 페이지 번호
-
+    $offset = $list_cnt * ($page_num - 1); // 오프셋
+    $prev_page_num = ($page_num - 1) < 1 ? 1 : ($page_num - 1); // 이전 버튼 페이지 번호
+    $next_page_num = ($page_num + 1) > $max_page_num ? $max_page_num : ($page_num + 1); // 다음 버튼 페이지 번호
 
     // 게시글 리스트 조회
     $arr_param = [
@@ -34,15 +31,13 @@ try {
 } catch (\Throwable $e) {
     echo $e->getMessage();
     exit;
-}
-finally {
+} finally {
     // PDO 파기
     if(!empty($conn)) {
         $conn = null;
     }
-} 
+}
 ?>
-
 
 <!DOCTYPE html>
 <html lang="ko">
@@ -55,6 +50,7 @@ finally {
 <body>
     <!-- 헤더 호출 -->
     <?php require_once(FILE_HEADER); ?>
+
     <main>
         <div class="main-top">
             <a href="./insert.php" class="a-button">글 작성</a>
@@ -65,14 +61,14 @@ finally {
                 <div class="board-title">게시글 제목</div>
                 <div class="board-createdat">작성 일자</div>
             </div>
-            <?php
+            <?php 
             foreach($result as $item) {
             ?>
-            <div class="item">
-                <div class="item-no"><?php echo $item["no"] ?></div>
-                <div class="item-title"><a href="./detail.php?no=<?php echo $item["no"] ?>&page=<?php echo $page_num ?>"><?php echo $item["title"] ?></a></div>
-                <div class="item-createdat"><?php echo $item["created_at"] ?></div>
-            </div>
+                <div class="item">
+                    <div class="item-no"><?php echo $item["no"] ?></div>
+                    <div class="item-title"><a href="./detail.php?no=<?php echo $item["no"] ?>&page=<?php echo $page_num ?>"><?php echo $item["title"] ?></a></div>
+                    <div class="item-createdat"><?php echo $item["created_at"] ?></div>
+                </div>
             <?php
             }
             ?>
@@ -80,14 +76,14 @@ finally {
         <div class="main-bottom">
             <a href="./list.php?page=<?php echo $prev_page_num ?>" class="a-button small-button">이전</a>
             <?php
-            for($num = 1; $num <= $max_page_num; $num++){
+            for($num = 1; $num <= $max_page_num; $num++) {
             ?>
-            <a href="./list.php?page=<?php echo $num ?>" class="a-button small-button"><?php echo $num ?></a>
+                <a href="./list.php?page=<?php echo $num ?>" class="a-button small-button"><?php echo $num ?></a>
             <?php
             }
             ?>
             <a href="./list.php?page=<?php echo $next_page_num ?>" class="a-button small-button">다음</a>
         </div>
     </main>
-</body> 
-</html> 
+</body>
+</html>
