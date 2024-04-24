d486ca560c3084d6a1f03d5c9d82eacfd17da48e
jubiber <myemail@example.com>
2024-04-24 20:07:58 +0900

    240424

--- JavaScript/tng/50.js
+++ JavaScript/tng/50.js
@@ -1,18 +1,12 @@
+
+// 2-2. 들킨 상태에서는 알러트가 안나옵니다. 안 들켰으면 계속 알러트가 나와야 합니다.
+
+
 // 1. 버튼 클릭 시 아래 내용의 알러트가 나옵니다.
     // "안녕하세요."
     // "숨어있는 div를 찾아보세요."
-// 2-1. 특정 영역에 마우스 포인터가 진입하면 아래 내용의 알러트가 나옵ㄴ다.
-    // "두근두근"
-// 2-2. 들킨 상태에서는 알러트가 안나옵니다. 안 들켰으면 계속 알러트가 나와야 합니다.
-
-// 3. 2번 영역을 클릭하면 아래의 알러트를 출력하고, 배경색이 베이지 색으로 바뀌어 나타납니다.
-    // "들켰다!"
-//4. 3번의 상태에서 다시 한번더 클릭하면 아래의 알러트를 출력하고, 베경색이 흰색으로 바귀어 안보이게 됩니다.
-    // "다시 숨는다."
-
-
-// 1.
 const BTN1 = document.querySelector('#btn1');
+
 BTN1.addEventListener('click', () => 
 (alert('"안녕하세요"')));
 BTN1.addEventListener('click', test);
@@ -25,33 +19,19 @@ BTN1.removeEventListener('click', test);
 function test() {
     alert('sdf');
 }
-//////////////
-
-// 2-1. 특정 영역에 마우스 포인터가 진입하면 아래 내용의 알러트가 나옵ㄴ다.
+// 2-1. 특정 영역에 마우스 포인터가 진입하면 아래 내용의 알러트가 나옵니다.
     // "두근두근"
 
-
-
-
-// const BTN_MODAL = document.querySelector('#btn1');
-// BTN_MODAL.addEventListener('click', () => {
-//     const MODAL_CONTAINER = document.querySelector('.modal-container');
-//     MODAL_CONTAINER.classList.toggle('display-none');
-// });
-// const divElement = document.querySelector('.modal-item');
-
-// divElement.addEventListener('mouseenter', () => {
-//     alert('"두근두근"');
-// });
-
-
-const divElement = document.querySelector('#specific-area');
+const divElement = document.querySelector('.item');
 
 // 특정 영역에 마우스 진입시 알림을 한 번만 표시하는 함수 정의
 function showAlertOnce() {
     alert('"두근두근"');
     divElement.removeEventListener('mouseenter', showAlertOnce);
 }
+divElement.addEventListener('mouseleave', e => {
+    e.target.style.backgroundColor = 'beige';
+});
 // 특정 영역에 마우스 진입 이벤트 리스너를 추가함.
 divElement.addEventListener('mouseenter', showAlertOnce);
 
@@ -61,25 +41,35 @@ divElement.addEventListener('mouseup', e => {
 });
 
 
-// 3. 마우스 클릭 ->  들켰다
+// 3. 2번 영역을 클릭하면 아래의 알러트를 출력하고, 배경색이 베이지 색으로 바뀌어 나타납니다.
+    // "들켰다!"
 
 divElement.addEventListener('mousedown', () => {
      alert('"들켰다!"');
      divElement.style.backgroundColor = 'white';
 });
 divElement.addEventListener('mouseup', () => {
-    divElement.style.backgroundColor = 'initial';
+    divElement.style.backgroundColor = 'orange';
 });
 // divElement.addEventListener('mouseup', e => {
 //     e.target.style.backgroundColor = '#fff';
 // });
+
+
+
 //4. 3번의 상태에서 다시 한번더 클릭하면 아래의 알러트를 출력하고, 베경색이 흰색으로 바귀어 안보이게 됩니다.
     // "다시 숨는다."
+
+
 divElement.addEventListener('mousedown', function(){
     alert('"다시 숨는다."');
 });
 divElement.removeEventListener('mousedown', test);
 
+
 function test() {
     alert('sdf');
 }
+// 보완: 마지막에 색깔 사라져야하는거
+// 3번이랑 4번 사이에 텀두게
+// 2-2 충족하게
