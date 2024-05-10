3893627d1a498809b3fbed91adbd22d915de04ce
jubiber <j832ch@naver.com>
2024-05-10 12:52:41 +0900

    240510 laravel_edu complete

--- laravel_edu/app/Http/Controllers/UserController.php
+++ laravel_edu/app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
@@ -73,7 +74,112 @@ class UserController extends Controller
                     ->orderBy('name', 'ASC')
                     ->get();
 
-        // WHERE (많)
+        // select * from users where id in (?, ?); [2, 5]
+        $result = DB::table('users')
+                    ->whereBetween('id', [2, 5])
+                    ->get();
+
+        // select * from users where deleted_at is null;;
+        $result = DB::table('users')
+                    ->whereNull('deleted_at')
+                    ->get();
+
+        // 2023년에 가입한 사람만 출력
+        // select * from users where year(created_at) = ?
+        // select * from users created_at beetween 20230101000000 and 202312312359
+        $result = DB::table('users')
+                    ->whereYear('created_at', '2023')
+                    ->get();
+
+        // 남자 회원의 수를 구하자
+        $result = DB::table('users')
+                    ->where('gender','M')
+                    ->get();
+        // 성별 회원수
+        // SELECT gender, COUNT(gender) FROM users GROUP BY gender;
+        $result = DB::table('users')
+                    ->select('gender', DB::raw('COUNT(gender) cnt'))
+                    ->groupBy('gender')
+                    ->having('gender', '=', 'M')
+                    ->get();
+
+        // select id, name from users order by id limit? offset ?; [1, 2];
+        $result = DB::table('users')
+                    ->select('id', 'name')
+                    ->orderBy('id')
+                    ->limit(1)
+                    ->offset(2)
+                    ->get();
+        $reqData = ''; // 유저가 1또는 2인 데이터 전달
+        $result = DB::table('users')
+                    ->when($reqData, function($query, $reqData) {
+                        $query->where('id', $reqData);
+                    })
+                    ->get();
+
+        // first() : 쿼리 결과에서 가장 첫번째 레코드만 반환 
+        $result = DB::table('users')->first();
+        // count() : 쿼리 결과의 레코드 수를 반환        
+        $result = DB::table('users')->count();
+        // find($id) : 지정된 기본키의 해당하는 레코드를 반환
+        // $result = DB::table('users')->fine(6);
+
+        // insert
+        // $result = DB::table('users')->insert([
+        //     'name' => '김영희'
+        //     ,'email' => 'kim@damin.com'
+        //     ,'password' => Hash::make('qwer1234!')
+        //     ,'gender' => 'F'
+
+        // ]);
+
+        // update
+        $result = DB::table('users')
+                    ->where('id', 11)
+                    ->update([
+                        'email' => 'kim@naver.com'
+                    ]);
+
+        // delete
+        // $result = DB::table('users')
+        //             ->where('id', 11)
+        //             ->delete();
+
+        // ---------------
+        // Eloquent Model
+        // ---------------
+        // $result = User::all();
+        // $result = User::find(6);
+
+        // upsert
+        $data = [
+            'name'       => '김영희'
+            ,'email'     => 'kim@naver.com'
+            ,'password'  => Hash::make('qwer1234!')
+            ,'gender'    => 'F'
+        ];
+        // $result = User::create($data);
+        
+
+        // update (최신 정보 추가)
+        // transaction (한꺼번에 수행되어야 할 일련의 연산모음)
+        // DB::beginTransaction();
+        // $target = User::find(14);
+        // $target->gender = 'M';
+        // save -> true false 뜸
+        // $result = $target->save();
+        // DB::commit();
+
+        // delete
+        // $result = User::destroy(14);
+
+        // 소프트 딜리트 된 데이터 조회
+        $result = User::withTrashed()->get(); // 소프트 딜리트 포함
+        $result = User::onlyTrashed()->get(); // 소프트 딜리트만
+
+        // 소프트 딜리트 된거 복원
+        $result = User::where('id', 10)->restore();   
+        
         return var_dump($result);
     }
 }
