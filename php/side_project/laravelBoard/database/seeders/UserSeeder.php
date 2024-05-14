92f99cd6ad8bf44dc99c6edaa12b949c2f1186f0
jubiber <myemail@example.com>
2024-05-14 21:36:44 +0900

    240514

--- php/side_project/laravelBoard/database/seeders/UserSeeder.php
+++ php/side_project/laravelBoard/database/seeders/UserSeeder.php
@@ -16,7 +16,7 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-         User::factory(10)->create();
+         User::factory(20)->create();
         // $arr = [
         //     'name'  => '관리자'
         //     ,'email' => 'admin@admin.com'
