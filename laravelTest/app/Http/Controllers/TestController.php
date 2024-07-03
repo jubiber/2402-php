b5dc0c86feabadfa12dbcadae83327304271e99a
jubiber <j832ch@naver.com>
2024-07-03 16:24:01 +0900

    240703

--- laravelTest/app/Http/Controllers/TestController.php
+++ laravelTest/app/Http/Controllers/TestController.php
@@ -11,3 +11,20 @@ class TestController extends Controller
         return view('test');
     }
 }
+
+function my_sum($num1, $num2) {
+    return $num1 + $num2;
+}
+
+echo my_sum(3, 5);
+
+$str = " sdsd ";
+echo trim($str);
+
+$str = "abcd";
+echo strtoupper($str);
+
+$str = "";
+$str = null;
+var_dump(isset($str1));
+
